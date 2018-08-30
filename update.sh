6bc600742a0420f80b952c737afd6f331abb6976
iconboot <iconboot@users.noreply.github.com>
2018-08-30 20:33:40 +0200

    Added the files

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
-./remove.sh
-./packages.sh
-
-dpkg-scanpackages -m . /dev/null >Packages
-bzip2 Packages
+#!/bin/sh
+apt-ftparchive packages ./ > ./Packages
+sed -i -e '/^SHA/d' ./Packages
+bzip2 -c9k ./Packages > ./Packages.bz2
+printf "Origin: iconboot Repo\nLabel: iconboot\nSuite: stable\nVersion: 1.0\nCodename: iconboot\nArchitecture: iphoneos-arm\nComponents: main\nDescription: iconboot Tweaks\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages.bz2 --printf="%s")" Packages.bz2\n" >Release;
+exit 0
