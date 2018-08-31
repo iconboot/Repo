31573da9ce45f749837e7f450c21d246b3781203
iconboot <iconboot@users.noreply.github.com>
2018-08-31 04:16:19 +0200

    Repo Setup

--- update.sh
+++ update.sh
@@ -1,6 +1,37 @@
-#!/bin/sh
-apt-ftparchive packages ./ > ./Packages
-sed -i -e '/^SHA/d' ./Packages
-bzip2 -c9k ./Packages > ./Packages.bz2
-printf "Origin: iconboot Repo\nLabel: iconboot\nSuite: stable\nVersion: 1.0\nCodename: iconboot\nArchitecture: iphoneos-arm\nComponents: main\nDescription: iconboot Tweaks\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages.bz2 --printf="%s")" Packages.bz2\n" >Release;
-exit 0
+rm ./Packages
+dpkg-scanpackages -m . /dev/null >Packages
+#./dpkg-scanpackages debs / > Packages
+rm ./Packages.bz2
+bzip2 -fks Packages
+
+
+# #!/bin/sh
+#
+# all: Release
+# .PHONY: Packages Packages.bz2 Release
+#
+# Release: Packages Packages.bz2
+# 	$(eval SIZE1 := $(shell wc -c Packages | sed 's/[[:space:]].*//'))
+# 	$(eval SIZE2 := $(shell wc -c Packages.bz2 | sed 's/[[:space:]].*//'))
+# 	@cp Release_ $@
+#
+# 	@echo MD5Sum: >> $@
+# 	@echo " `md5sum Packages | sed "s/  / $(SIZE1) /"`" >> $@
+# 	@echo " `md5sum Packages.bz2 | sed "s/  / $(SIZE2) /"`" >> $@
+#
+# 	@echo SHA1: >> $@
+# 	@echo " `sha1sum Packages | sed "s/  / $(SIZE1) /"`" >> $@
+# 	@echo " `sha1sum Packages.bz2 | sed "s/  / $(SIZE2) /"`" >> $@
+#
+# 	@echo SHA256: >> $@
+# 	@echo " `sha256sum Packages | sed "s/  / $(SIZE1) /"`" >> $@
+# 	@echo " `sha256sum Packages.bz2 | sed "s/  / $(SIZE2) /"`" >> $@
+#
+# Packages:
+# 	@./dpkg-scanpackages -m . /dev/null > Packages
+#
+# Packages.bz2: Packages
+# 	@bzip2 -fk $^
+#
+# clean:
+# 	@rm -rf Packages Packages.bz2 Release &> /dev/null
